1139512ac7c08f7755f6330608ec51038f1c473a
Jefferson Nascimento <jp.nascimento@gmail.com>
2017-03-25 13:30:05 -0300

    Mudanças

--- infra/ItemDao.js
+++ infra/ItemDao.js
@@ -1,14 +1,20 @@
-function ItemDao (model) {
-    this._model = model;
+function ItemDao (connection) {
+    this._connection = connection;
 }
 
-ItemDao.prototype.list = function(callback) {
-    this._model.find(callback);
+// Funçao para listar os livros
+ItemDao.prototype.lista = function (callback) {
+    this._connection.query('select * from livros', callback);
 }
 
-ItemDao.prototype.save = function (item, callback) {
-    var livroModel = new this._model(item);
-    itemModel.save(callback);
+// Função para inserir novo livro
+ItemDao.prototype.salva = function (livro, callback) {
+    this._connection.query('insert into livros (titulo, preco, descricao) values (?, ?, ?)', [livro.titulo, livro.preco, livro.descricao], callback);
+}
+
+// Funçao que busca por id
+ItemDao.prototype.pesquisa = function(id, callback) {
+    this._connection.query('select * from livros where id = ?', [ id ], callback);
 }
 
 module.exports = function() { return ItemDao };
--- infra/connectionFactory.js
+++ infra/connectionFactory.js
@@ -1,20 +1,20 @@
 var mysql = require('mysql');
 
-var databaseName = 'casadocodigo';
+var databaseName = 'fisiofit_sell_test';
 
 // Configuração para teste
-if (process.env.NODE_ENV == 'test') {
-    databaseName = 'casadocodigo_teste';
+if (process.env.NODE_ENV == 'prod') {
+    databaseName = 'fisiofit_sell';
 }
 
 var pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'root123',
     database: databaseName,
     connectionLimit: 15
 });
-    
+
 
 function createConnection() {
 
